b841088ad4f7a2d14be9ae1bcfa33584a5b4ebe1
Mavchi <aleksi.khlebnikov@gmail.com>
2021-02-05 18:05:38 +0200

    5.2: blogilistan frontend, step2

--- front/src/App.js
+++ front/src/App.js
@@ -22,12 +22,20 @@ const App = () => {
     try {
       const user = await loginServices.login({ username, password })
 
+      window.localStorage.setItem(
+        'loggedBlogsPart5User', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const logOut = () => {
+    window.localStorage.removeItem('loggedBlogsPart5User')
+    setUser(null)
   }
 
   if (user === null)
@@ -60,7 +68,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={logOut}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
